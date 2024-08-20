19bd1a3ca5bf4c3b097e6c2c70e0a94c82fc39e6
Miguel Arriaga Velasco <marriagav3@gmail.com>
2024-08-20 15:48:32 -0600

    name

--- Project.swift
+++ Project.swift
@@ -10,6 +10,7 @@ let project = Project(
             bundleId: "com.mav.taskchamp",
             infoPlist: .extendingDefault(
                 with: [
+                    "CBBundleDisplayName": "Taskchamp",
                     "CFBundleVersion": "2",
                     "UILaunchScreen": [
                         "UIColorName": "LaunchBackground"
