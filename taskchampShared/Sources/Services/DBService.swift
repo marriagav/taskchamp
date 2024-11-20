320bbcf87c49b263a4836cf54cf78d265f67db57
Miguel Arriaga Velasco <marriagav3@gmail.com>
2024-11-19 23:25:28 -0500

    update macos version

--- taskchampShared/Sources/Services/DBService.swift
+++ taskchampShared/Sources/Services/DBService.swift
@@ -4,8 +4,8 @@ import WidgetKit
 
 public class DBService {
     enum TasksColumns {
-        static let uuid = Expression<String>("uuid")
-        static let data = Expression<String>("data")
+        static let uuid = Expression<String>(value: "uuid")
+        static let data = Expression<String>(value: "data")
     }
 
     public static let shared = DBService()
