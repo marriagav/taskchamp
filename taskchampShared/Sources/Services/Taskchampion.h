0dd3b1fc74169f0d88cf701536edcd5818bc91f7
Miguel Arriaga Velasco <marriagav3@gmail.com>
2025-03-15 19:36:02 -0400

    gitignore

---
+++ taskchampShared/Sources/Services/Taskchampion.h
@@ -0,0 +1,15 @@
+//
+//  TaskchampionService.h
+//  taskchamp
+//
+//  Created by Miguel Arriaga Velasco on 15/03/25.
+//
+
+#import <Foundation/Foundation.h>
+#include "lib.rs.h"
+
+@interface Taskchampion : NSObject
+
+- (void)printAnExample;
+
+@end
