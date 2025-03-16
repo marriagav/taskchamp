ec1b7be148e8eb603d12db07feb1c4340fe9c2c2
Miguel Arriaga Velasco <marriagav3@gmail.com>
2025-03-15 20:48:05 -0400

    2.0

--- taskchampShared/Sources/Services/TaskchampionService.swift
+++ taskchampShared/Sources/Services/TaskchampionService.swift
@@ -1,13 +1,6 @@
-// import taskchampion
-// import Taskchampion
-// TODO: THIS WHOLE FILE
 import Taskchampion
+
 public func printAnExample() -> String {
-//    var replica = taskchampion.tc.new_replica_in_memory()
-//    let task = taskchampion.tc.get_all_task_data(&replica)
-//    print("task", task)
-//    return "HELLO \(taskchampion.tc.Uuid())"
-//    test.printAnExample()
     print("hello uuid", Taskchampion.uuid_v4())
     let replica = Taskchampion.new_replica_in_memory()
     var tasks = replica.all_task_data()!
