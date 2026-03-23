8b14d5e14d20caa85b24a91d6f90150db1832e0d
Miguel Arriaga <marriagav3@gmail.com>
2026-03-23 09:51:49 -0600

    release (#103)
    * glass attempt 1
    
    * glass attempt 2
    
    * increase version number
    
    * Migration (#74)
    
    * create wall
    
    * create wall
    
    * create wall
    
    * add tags and nlp improvements
    
    * release notes
    
    * tag integration
    
    * tag integration fix
    
    * tag integration fix
    
    * remove wall from obsidian settings
    
    * filter invalid tags
    
    * new obsidian settings view
    
    * bugfix cache for tags
    
    * oops
    
    * oops
    
    * oops
    
    * oops
    
    * create file from file system
    
    * sync storekit
    
    * markdown editor
    
    * markdown editor
    
    * save markdown
    
    * save markdown
    
    * save markdown
    
    * attempt fix grandpas
    
    * attempt fix grandpas
    
    * - (#86)
    
    * Update Project.swift (#91)
    
    * Fix README (#89)
    
    * new sync methods
    
    * new sync methods
    
    * Fix formatting of note in README.md
    
    * Revise sync troubleshooting steps for Taskchamp
    
    Updated instructions for syncing issues with Taskchamp.
    
    * readme
    
    * gitignore
    
    * Update CONTRIBUTING.md
    
    * update cicd
    
    * bump version
    
    * Check taskNoteFolderBookmark instead of obsidianVaultName
    
    ---------
    
    Co-authored-by: Zik <vladziku@gmail.com>
    Co-authored-by: Florian Heiderich <florian@heiderich.org>

--- scripts/clone_taskchampion_swift.sh
+++ scripts/clone_taskchampion_swift.sh
@@ -1,10 +1,32 @@
+#!/bin/bash
 set -e
 
-# Get the script's directory
-SCRIPT_DIR=$(dirname "$0")
+CI=false
+while [[ "$#" -gt 0 ]]; do
+    case $1 in
+        --ci) CI=true ;;
+        *) echo "Unknown parameter: $1"; exit 1 ;;
+    esac
+    shift
+done
 
-# Change to the directory where the script is located
-cd "$SCRIPT_DIR"
+SCRIPT_DIR=$(cd "$(dirname "$0")"; pwd)
+cd "$SCRIPT_DIR/.."
 
-cd ..
-git clone git@github.com:marriagav/task-champion-swift.git # Using the fork for now before we are able to import with the new structure
+if [ "$CI" = true ]; then
+    if [ -z "$DEPLOY_KEY" ]; then
+        echo "Error: DEPLOY_KEY not set"
+        exit 1
+    fi
+
+    SSH_DIR="$HOME/.ssh"
+    mkdir -p "$SSH_DIR"
+    echo "$DEPLOY_KEY" > "$SSH_DIR/id_ed25519"
+    chmod 600 "$SSH_DIR/id_ed25519"
+    ssh-keyscan github.com > "$SSH_DIR/known_hosts"
+
+    git clone git@github.com:marriagav/task-champion-swift.git
+else
+    git clone git@github.com:marriagav/task-champion-swift.git
+fi
+
